62f30de545a15efa7a6cc5bb8964dcfef92076fe
Paulo computador trabalho <paulo.dev1401@gmail.com>
2023-10-20 15:33:17 -0300

    challenger ok

---
+++ src/Contador/ParametrosInvalidosException.java
@@ -0,0 +1,15 @@
+package Contador;
+
+public class ParametrosInvalidosException extends Exception{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String mensagem) {
+		super(mensagem);
+		
+	}
+}
